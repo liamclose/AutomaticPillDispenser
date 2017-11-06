18d9407d977938f07c9b2befeddaaa2e0e2f79c6
owen <awowenlee@gmail.com>
2017-11-06 13:29:40 -0500

    able to submit data through a form into a java object and pass it back into a new page

---
+++ src/main/java/com/sysc3010/m7/model/PatientForm.java
@@ -0,0 +1,23 @@
+package com.sysc3010.m7.model;
+
+
+public class PatientForm {
+	private String name;
+
+	public PatientForm() {
+
+	}
+
+	public PatientForm(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
